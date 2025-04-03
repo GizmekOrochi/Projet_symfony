4243b4940be114c5eef7431f0dff5ca64a4e9108
GizmekOrochi <valcurion369@gmail.com>
2025-04-03 19:50:18 +0200

    ygvbnjgfvbnjuytfvbhjuyg

--- src/Controller/CartController.php
+++ src/Controller/CartController.php
@@ -28,44 +28,62 @@ class CartController extends AbstractController
         $session = $request->getSession();
         $cart = $session->get('cart', []);
 
-        $product = $em->getRepository(FootballPlayer::class)->find($id);
-        if (!$product) {
-            $this->addFlash('error', 'Product not found.');
+        if (isset($cart[$id])) {
+            $cart[$id]['quantity']++;
+            $session->set('cart', $cart);
+            $this->addFlash('success', 'Product quantity updated in cart.');
             return $this->redirectToRoute('app_cart');
         }
 
-        if (isset($cart[$id])) {
-            $cart[$id]['quantity']++;
-        } else {
-            $cart[$id] = [
-                'product' => [
-                    'id' => $product->getId(),
-                    'name' => $product->getName(),
-                    'firstName' => $product->getFirstName(),
-                    'valueMoney' => $product->getValueMoney(),
-                    'currentClub' => $product->getCurrentClub(),
-                    'currentRole' => $product->getCurrentRole(),
-                ],
-                'quantity' => 1,
-            ];
+        $product = $em->getRepository(FootballPlayer::class)->find($id);
+        if (!$product) {
+            $this->addFlash('error', 'Product not found or already reserved.');
+            return $this->redirectToRoute('app_cart');
         }
 
+        $cart[$id] = [
+            'product' => [
+                'id' => $product->getId(),
+                'name' => $product->getName(),
+                'firstName' => $product->getFirstName(),
+                'valueMoney' => $product->getValueMoney(),
+                'currentClub' => $product->getCurrentClub(),
+                'currentRole' => $product->getCurrentRole(),
+            ],
+            'quantity' => 1,
+        ];
+
+        $em->remove($product);
+        $em->flush();
+
         $session->set('cart', $cart);
-        $this->addFlash('success', 'Product added to cart.');
+        $this->addFlash('success', 'Product added to cart and removed from the database.');
 
         return $this->redirectToRoute('app_cart');
     }
 
     #[Route(path: '/cart/remove/{id}', name: 'app_cart_remove')]
-    public function remove(Request $request, int $id): Response
+    public function remove(Request $request, int $id, EntityManagerInterface $em): Response
     {
         $session = $request->getSession();
         $cart = $session->get('cart', []);
 
         if (isset($cart[$id])) {
+            $item = $cart[$id];
+            for ($i = 0; $i < $item['quantity']; $i++) {
+                $player = new FootballPlayer();
+                $player->setName($item['product']['name']);
+                $player->setFirstName($item['product']['firstName']);
+                $player->setValueMoney($item['product']['valueMoney']);
+                $player->setCurrentClub($item['product']['currentClub']);
+                $player->setCurrentRole($item['product']['currentRole']);
+                $em->persist($player);
+            }
+            $em->flush();
+
             unset($cart[$id]);
             $session->set('cart', $cart);
-            $this->addFlash('success', 'Product removed from cart.');
+            $this->addFlash('success', 'Product removed from cart and re-added to the database.');
         } else {
             $this->addFlash('error', 'Product not found in cart.');
         }
@@ -74,11 +92,25 @@ class CartController extends AbstractController
     }
 
     #[Route(path: '/cart/clear', name: 'app_cart_clear')]
-    public function clear(Request $request): Response
+    public function clear(Request $request, EntityManagerInterface $em): Response
     {
         $session = $request->getSession();
+        $cart = $session->get('cart', []);
+
+        foreach ($cart as $id => $item) {
+            for ($i = 0; $i < $item['quantity']; $i++) {
+                $player = new FootballPlayer();
+                $player->setName($item['product']['name']);
+                $player->setFirstName($item['product']['firstName']);
+                $player->setValueMoney($item['product']['valueMoney']);
+                $player->setCurrentClub($item['product']['currentClub']);
+                $player->setCurrentRole($item['product']['currentRole']);
+                $em->persist($player);
+            }
+        }
+        $em->flush();
         $session->remove('cart');
-        $this->addFlash('success', 'Cart cleared.');
+        $this->addFlash('success', 'Cart cleared and all products re-added to the database.');
 
         return $this->redirectToRoute('app_cart');
     }
@@ -93,17 +125,8 @@ class CartController extends AbstractController
             $this->addFlash('error', 'Your cart is empty.');
             return $this->redirectToRoute('app_cart');
         }
-
-        foreach ($cart as $id => $item) {
-            $player = $em->getRepository(FootballPlayer::class)->find($id);
-            if ($player) {
-                $em->remove($player);
-            }
-        }
-
-        $em->flush();
         $session->remove('cart');
-        $this->addFlash('success', 'Purchase successful.');
+        $this->addFlash('success', 'Purchase successful. Products have been purchased and remain removed from the database.');
 
         return $this->redirectToRoute('app_cart');
     }
--- src/Controller/SuperAdminController.php
+++ src/Controller/SuperAdminController.php
@@ -6,6 +6,7 @@ use App\Entity\User;
 use App\Repository\UserRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
@@ -88,5 +89,48 @@ class SuperAdminController extends AbstractController
             $this->addFlash('info', 'Cet utilisateur n\'est pas Admin.');
         }
         return $this->redirectToRoute('super_admin_users');
+    }
+
+    #[Route('/super-admin/create-user', name: 'super_admin_create_user')]
+    public function createUser(Request $request): Response
+    {
+        if ($request->isMethod('POST')) {
+            $login    = $request->request->get('login');
+            $name     = $request->request->get('name');
+            $country  = $request->request->get('country');
+            $password = $request->request->get('password');
+            $role     = $request->request->get('role');
+
+            if (empty($login) || empty($name) || empty($country) || empty($password) || empty($role)) {
+                $this->addFlash('error', 'Please fill in all required fields.');
+                return $this->redirectToRoute('super_admin_create_user');
+            }
+
+            if (!in_array($role, ['ROLE_ADMIN', 'ROLE_CLIENT'], true)) {
+                $this->addFlash('error', 'Invalid role selected.');
+                return $this->redirectToRoute('super_admin_create_user');
+            }
+
+            $existingUser = $this->userRepository->findOneBy(['login' => $login]);
+            if ($existingUser) {
+                $this->addFlash('error', 'Login already taken.');
+                return $this->redirectToRoute('super_admin_create_user');
+            }
+
+            $user = new User();
+            $user->setLogin($login);
+            $user->setName($name);
+            $user->setCountry($country);
+            $user->setPassword(password_hash($password, PASSWORD_DEFAULT));
+            $user->setRoles([$role]);
+
+            $this->entityManager->persist($user);
+            $this->entityManager->flush();
+
+            $this->addFlash('success', 'User created successfully.');
+            return $this->redirectToRoute('super_admin_users');
+        }
+
+        return $this->render('super_admin/create_user.html.twig');
     }
 }
